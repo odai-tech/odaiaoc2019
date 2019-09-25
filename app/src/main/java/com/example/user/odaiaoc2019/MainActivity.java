297f5582db6e03cfcb9b0ba7c8d09f306369e322
odai-tech <odai195@gmail.com>
2019-09-25 11:00:12 +0300

    aa

---
+++ app/src/main/java/com/example/user/odaiaoc2019/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.user.odaiaoc2019;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    //1, properties definition
+    EditText editTextEmail , editTextPassword;
+    Button buttonLogIn, buttonSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //2.initialize properties
+        editTextEmail=findViewById(R.id.editTextEmail);
+        editTextPassword=findViewById(R.id.editTextPassword);
+
+        buttonLogIn=findViewById(R.id.buttonLogIn);
+        buttonSignUp=findViewById(R.id.buttonSignUp);
+        buttonLogIn.setOnClickListener(this);
+        buttonSignUp.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==buttonLogIn){
+            if(editTextEmail.getText().toString().equals("")|| editTextPassword.getText().toString().equals("")){
+                Toast.makeText(this,"Empty Password or Email",Toast.LENGTH_LONG).show();
+            }
+            else{ Intent i=new Intent(this,MainActivity.class);
+                i.putExtra("email" , editTextEmail.getText().toString());
+                i.putExtra("password" , editTextPassword.getText().toString());
+                startActivity(i);}
+
+        }
+        else{
+            Intent i=new Intent(this,SignUpActivity.class);
+            startActivity(i);
+
+        }
+    }
+}
