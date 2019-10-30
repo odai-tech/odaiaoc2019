5ec92d1e9e553b9406b173f684376e35d2b5e67c
odai-tech <odai195@gmail.com>
2019-10-30 10:31:23 +0200

    aa

---
+++ app/src/main/java/com/example/user/odaiaoc2019/HospitalUI.java
@@ -0,0 +1,13 @@
+package com.example.user.odaiaoc2019;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HospitalUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospital_ui);
+    }
+}
